979ed49abfebc45c5c55dad32edee9f16e7b5151
JurajMarincic <juraj.marincic1@gmail.com>
2024-02-26 19:47:32 +0100

    Add entity framework, add category entity,

---
+++ Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookWebshop.Models;
+
+public class Category
+{
+    // primarni kljuc
+    [Key]
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+}
